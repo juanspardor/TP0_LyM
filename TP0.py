0ed9babc83c6611b3c660c8d9e5e17857daf84ca
juanspardor <j.pardor@uniandes.edu.co>
2022-08-16 23:34:49 -0500

    Comentarios e ideas de solucion agregados. Nada implementado

--- TP0.py
+++ TP0.py
@@ -3,6 +3,24 @@
 
 import sys
 
+#Valor que se retorna al final de revisar el archivo. True si es valido, False d.l.c
+valido = True
+
+
+#Al leer la primera linea, revisar que sea PROG. D.l.c valido = false
+#Al leer la ultima linea, revisar que sea GORP. D.l.c valido = false
+
+
+#Tener un diccionario con las variables donde la llave es el nombre y el valor es el valor asignacio. Iniciamos con valor = null
+
+#Tener un diccionario con los metodos, donde la llave es el nombre y el valor es el numero de parametros. 
+#Cuando se haga el llamado a un metodo se revisa que exista la llave y que el numero de parametros agregados sea igual el valor que esta en el dict
+
+#Cuando inicia un metodo, asegurarse que la siguiente linea sea '{'. D.l.c valido = false.
+#Quizas tener un booleano que inidique si se abrieron los parentesis de un metodo, pero no estoy seguro de como hacer que no se putee si nunca se cierre
+
+
+
 
 
 
